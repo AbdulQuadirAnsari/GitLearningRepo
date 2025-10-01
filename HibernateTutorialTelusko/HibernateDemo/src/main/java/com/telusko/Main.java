1fb2629429ff139a76531616d29943fb94154c3b
Abdul Quadir Ansari <143abdulquadir@gmail.com>
2025-10-01 19:18:16 +0530

    learning hibernate from telusko #3

--- HibernateTutorialTelusko/HibernateDemo/src/main/java/com/telusko/Main.java
+++ HibernateTutorialTelusko/HibernateDemo/src/main/java/com/telusko/Main.java
@@ -2,15 +2,16 @@ package com.telusko;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
 public class Main {
     public static void main() {
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setName("Naveen");
-        a1.setTech("Java");
+        //Alien a1 = new Alien();
+        //a1.setAid(103);
+        //a1.setName("Kiran");
+        //a1.setTech("Gen AI");
 
         Configuration config = new Configuration();
         config.addAnnotatedClass(com.telusko.Alien.class);
@@ -20,7 +21,11 @@ public class Main {
 
         Session session = factory.openSession();
 
-        session.persist(a1);
+        Alien a1 = session.find(Alien.class, 103);
+        System.out.println(a1);
+
+        session.close();
+        factory.close();
 
     }
 }
